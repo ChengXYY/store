3553a8fa50b5a2d29dd1fe8063de36d685eb3454
ChengXYY <chengxyy@qq.com>
2019-06-03 14:01:54 +0800

    完成前端index页面所有内容填充

--- src/main/java/com/cy/store/client/PageController.java
+++ src/main/java/com/cy/store/client/PageController.java
@@ -30,60 +30,8 @@ public class PageController extends ClientConfig {
     public String index(ModelMap modelMap){
         pageData.put("title", "首页-"+systemTitle);
         pageData.put("topflag", "index");
-        pageData.put("intro", "翡翠");
-        pageData.put("subintro", "翡翠（jadeite）， 也称翡翠玉、翠玉、缅甸玉，是玉的一种。翡翠的正确定义是以硬玉矿物为主的辉石类矿物组成的纤维状集合体 ，但是翡翠并不等于硬玉");
-        pageData.put("toplink", "");
         modelMap.addAttribute("page", pageData);
         return webHtml+"index";
-    }
-
-    @RequestMapping(value = "/page/{param}")
-    public String page(@PathVariable("param") String param, ModelMap modelMap){
-        Sitepage sitepage = sitepageService.get(param);
-        pageData.put("title", "故事-"+systemTitle);
-        pageData.put("topflag", "index");
-        pageData.putAll(CommonOperation.obj2Json(sitepage));
-        modelMap.addAttribute("page", pageData);
-        return webHtml+"page";
-    }
-
-    @RequestMapping(value = "/story", method = RequestMethod.GET)
-    public String story(@RequestParam(value = "page", defaultValue = "1", required = false)Integer page,
-                        HttpServletRequest req,
-                        ModelMap modelMap){
-        /*
-        Map<String, Object> filter = new HashMap<>();
-        if(page == null || page<1){
-            page = 1;
-        }
-        int totalCount = sitepageService.getCount(filter);
-        int pageCount = (int)Math.ceil(totalCount/pageSize);
-        if(pageCount <1){
-            pageCount = 1;
-        }
-
-        filter.put("page", (page-1)*pageSize);
-        filter.put("pagesize", pageSize);
-
-        List<Sitepage> list = sitepageService.getList(filter); */
-
-
-        String requestUrl =  req.getRequestURI();
-
-        pageData.put("title", "故事-"+systemTitle);
-        pageData.put("topflag", "story");
-     //   pageData.put("list", list);
-        pageData.put("currentPage", page);
-        pageData.put("pageCount", 15);
-        pageData.put("totalCount", 143);
-        pageData.put("currentUrl", requestUrl);
-
-        pageData.put("intro", "翡翠");
-        pageData.put("subintro", "翡翠（jadeite）， 也称翡翠玉、翠玉、缅甸玉，是玉的一种。翡翠的正确定义是以硬玉矿物为主的辉石类矿物组成的纤维状集合体 ，但是翡翠并不等于硬玉");
-
-        modelMap.addAttribute("page", pageData);
-
-        return webHtml+"story";
     }
 
     @RequestMapping("/product")
@@ -99,11 +47,7 @@ public class PageController extends ClientConfig {
         pageData.put("pageCount", 15);
         pageData.put("totalCount", 143);
         pageData.put("currentUrl", requestUrl);
-
-        pageData.put("intro", "翡翠");
-        pageData.put("subintro", "翡翠（jadeite）， 也称翡翠玉、翠玉、缅甸玉，是玉的一种。翡翠的正确定义是以硬玉矿物为主的辉石类矿物组成的纤维状集合体 ，但是翡翠并不等于硬玉");
-
-        modelMap.addAttribute("page", pageData);
+  modelMap.addAttribute("page", pageData);
 
         return webHtml+"product";
     }
@@ -112,9 +56,6 @@ public class PageController extends ClientConfig {
     public String blog(ModelMap modelMap){
         pageData.put("title", "新闻-"+systemTitle);
         pageData.put("topflag", "blog");
-        pageData.put("intro", "翡翠");
-        pageData.put("toplink", "");
-        pageData.put("subintro", "翡翠（jadeite）， 也称翡翠玉、翠玉、缅甸玉，是玉的一种。翡翠的正确定义是以硬玉矿物为主的辉石类矿物组成的纤维状集合体 ，但是翡翠并不等于硬玉");
         modelMap.addAttribute("page", pageData);
         return webHtml+"blog";
     }
